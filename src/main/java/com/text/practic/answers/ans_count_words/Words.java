cb511e30eed0748cdd57cf8ae9bc0906f5c011e8
labisy <andreystepchenkov1@gmail.com>
2023-01-28 11:09:50 +0300

    create answers package

---
+++ src/main/java/com/text/practic/answers/ans_count_words/Words.java
@@ -0,0 +1,11 @@
+package com.text.practic.answers.ans_count_words;
+
+import java.util.List;
+
+public class Words {
+
+    public String countWords(List<String> target) {
+
+        return "";
+    }
+}
