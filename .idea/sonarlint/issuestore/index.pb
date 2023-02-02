eb5105798f36fbea8ac77f17d9f9f56fc31123af
andre <stepchenkovyt@gmail.com>
2023-02-02 12:49:24 +0300

    add kingdom

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,6 @@
 
-i
-9src/test/java/com/text/practic/count_words/WordsTest.java,2\9\29f8bddae6e887223f535ecc6ba90fc47e7161e4
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
---
+++ src/main/java/com/text/practic/answers/count_word/Words.java
@@ -0,0 +1,15 @@
+package com.text.practic.answers.count_word;
+
+import java.util.List;
+
+public class Words {
+
+    public String countWords(List<String> target) {
+
+        return "";
+    }
+
+    private List<String> getWordsFromText(List<String> list) {
+        return
+    }
+}
